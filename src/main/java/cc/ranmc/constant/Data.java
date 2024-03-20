4a9029c57c6731b54e28179f1829bd04c677624c
Ran <xyfwdy@gmail.com>
2024-03-20 14:25:57 +0800

    更新 版本号

--- src/main/java/cc/ranmc/constant/Data.java
+++ src/main/java/cc/ranmc/constant/Data.java
@@ -4,7 +4,7 @@ import cc.ranmc.util.DataFile;
 
 public class Data {
     public static final String AUTHOR = "Ranica";
-    public static final String VERSION = "3.3";
+    public static final String VERSION = "3.4";
     public static final String WEB_SITE = "https://www.ranmc.cc/";
     public static final String VERIFY_WEB_SITE = "https://www.ranmc.cc/verify.html?key=";
     public static final int PORT = 2263;
