127e0fe031cdce59cf5fd427acfe87c638287be4
Ran <xyfwdy@gmail.com>
2024-05-10 07:20:46 +0800

    更新 邮件内容

--- src/main/java/cc/ranmc/constant/Email.java
+++ src/main/java/cc/ranmc/constant/Email.java
@@ -2,20 +2,32 @@ package cc.ranmc.constant;
 
 public class Email {
     public static String text = """
-            <div>
-                <p>玩家 %player% 正在申请%action%</p>
-                <p>请确认是本人操作后<a href='%url%'>点击此处</a>链接确认
-                <p>如果您的邮箱程序不支持链接点击操作</p>
-                <p>请拷贝下方链接至您浏览器地址栏进入</p>
-                <p><a href='%url%'>%url%</a></p>
-                <p></p>
-                <p>-------------------</p>
-                <p></p>
-                <p>祝你游戏愉快</p>
-                <p>我的世界丨桃花源 呈上</p>
-                <p>服务器群号：<a href='http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=c3Mc6vrvjYh6KL8_ssFS7CVS5I8yejlp&authKey=0iI8Rw8rDgvxpanamKyJ7vJfMHDakbYS8ofxw%2BqQy9863RplPn91T3R7aHNBXQGV&noverify=0&group_code=429357720'>429357720</a></p>
-                <p>服务器地址：ranmc.cc</p>
-                <p>备用地址壹：b1.ranmc.cc</p>
-                <p>备用地址贰：b2.ranmc.cc</p>
-            </div>""";
+            <div style="background-color: #364f6b;display: flex;justify-content: center;align-items: center;flex-direction: column;height: 100%">
+                <br>
+                <div style="display: flex;flex-direction: column;align-items: center;">
+                    <img style="width: 340px;height: 176px;" src="https://z3.ax1x.com/2021/01/14/sUds6x.png"></img>
+                </div>
+                <div style="display: flex;flex-direction: column;">
+                    <p style="color: #eaeaea">
+                        玩家 %player% 正在申请%action%
+                        <br>请确认是本人操作后
+                        <a style="color: #3fc1c9" href='%url%'>点击确认</a>
+                        <br>如果您的邮箱程序不支持链接点击操作
+                        <br>请拷贝下方链接至您浏览器地址栏进入
+                        <br><br>
+                        <a style="color: #3fc1c9" href='%url%'>%url%</a>
+                    </p>
+                    <p style="color: #eaeaea">
+                        祝你游戏愉快
+                        <br>系统邮件，请勿回复
+                        <br>我的世界丨桃花源 呈上
+                        <br>服务器群号：<a style="color: #3fc1c9" href='http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=c3Mc6vrvjYh6KL8_ssFS7CVS5I8yejlp&authKey=0iI8Rw8rDgvxpanamKyJ7vJfMHDakbYS8ofxw%2BqQy9863RplPn91T3R7aHNBXQGV&noverify=0&group_code=429357720'>429357720</a>
+                        <br>服务器地址：ranmc.cc
+                        <br>备用地址壹：b1.ranmc.cc
+                        <br>备用地址贰：b2.ranmc.cc
+                    </p>
+                </div>
+                <br>
+            </div>
+            """;
 }
