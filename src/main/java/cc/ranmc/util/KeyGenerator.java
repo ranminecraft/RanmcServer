b8ecc420374820e5c8e6e714cfd700b7d301e0be
Ran <xyfwdy@gmail.com>
2024-06-10 17:47:54 +0800

    更新 验证码生成

--- src/main/java/cc/ranmc/util/KeyGenerator.java
+++ src/main/java/cc/ranmc/util/KeyGenerator.java
@@ -6,9 +6,9 @@ public class KeyGenerator {
 
     public static String get() {
         StringBuilder builder = new StringBuilder();
-        double length = (Math.random() * 6) + 6;
+        double length = (Math.random() * 2) + 4;
         for (int i = 0; i < length; i++) {
-            String content = "abcdefghijklmnopqrstuvwxyz0123456789";
+            String content = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
             int number = new Random().nextInt(content.length() - 1);
             builder.append(content.charAt(number));
         }
