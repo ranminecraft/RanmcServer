a9c5b6991171f84b7e03925269016a722dbed8e8
Ran <xyfwdy@gmail.com>
2025-12-19 09:30:46 +0800

    无视起床服务器

--- src/main/java/cc/ranmc/server/util/MinecraftUtil.java
+++ src/main/java/cc/ranmc/server/util/MinecraftUtil.java
@@ -37,7 +37,9 @@ public class MinecraftUtil {
                         JSONObject json = JSONObject.parseObject(record.toString());
                         String name = json.getString("name");
                         String srv = json.getString("value");
-                        if (name.startsWith("_minecraft._tcp.") && !name.contains("city")) {
+                        if (name.startsWith("_minecraft._tcp.")
+                                && !name.contains("bed")
+                                && !name.contains("city")) {
                             String serverName = name.replace("_minecraft._tcp.", "") + ".ranmc.cc";
                             serverStatusMap.put(serverName, isServerOnline(srv));
                             serverSrvMap.put(serverName, srv);
